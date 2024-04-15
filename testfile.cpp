1416e64cd882089a528c9695c4d6709e457403ba
anand vaibhav singh <anandvaibhav68@gmail.com>
2024-04-15 13:53:13 +0530

    added  a function to add two numbers testfile

---
+++ testfile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int f1(int x,int y);
+using namespace std;
+int main()
+{
+    cout<<"hello abhi";
+    cout<<f1(2,4);
+    return 0;
+}
+int f1(int x,int y)
+{
+    return x+y;
+}
